c45182d37bb3799725e9e73cd1942ae1dd62c779
Emily Scudero <emily@MSI.COM>
2021-02-07 21:53:48 -0500

    restaurant component added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+/*import React, { useEffect, useState } from 'react';*/
+
 import React, { useEffect, useState } from 'react';
+import Satellite from "./components/Satellite";
+import Restaurants from './components/Restaurants'
 
 function App() {
-  // const[longitude, setLongitude] = useState(longitude)
-  // const[latitude, setLatitude] = useState(latitude)
+    const[longitude, setLongitude] = useState()
+    const[latitude, setLatitude] = useState()
 
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    },[]);
 
+    function showPosition(position){
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    }
 
-useEffect(() => {
-  navigator.geolocation.getCurrentPosition(showPosition);
-},[]);
-
-
-function showPosition(position){
-  console.log(`latitude: ${position.coords.latitude} and longitude ${position.coords.longitude}`)
-}
-
-
-  return (
-<div>
-
-</div>
-
+    return (
+      <div>
+        <Satellite latitude={latitude} longitude={longitude}/>
+        <Restaurants latitude={latitude} longitude={longitude} />
+      </div>
   );
 }
 
 export default App;
+
+
+
+
+
