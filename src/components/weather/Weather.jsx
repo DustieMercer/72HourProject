aca1bcb89cb2c389ccce2f466ee2e9afbeb738e4
Dustie Mercer <d.nichole.mercer@gmail.com>
2021-02-09 20:11:41 -0500

    finished weather

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,72 +1,45 @@
 import React, { useState, useEffect } from "react";
 import {
   Card,
-  CardImg,
   CardTitle,
   Button,
   CardText,
-  ButtonDropdown, 
-  DropdownToggle, 
-  DropdownMenu, 
-  DropdownItem
 } from "reactstrap";
 import sunIcon from "../assets/sunIcon.png";
 
-const baseurl = "http://api.openweathermap.org/data/2.5/weather?lat=";
-const connector1 = "&lon=";
-const connectorFahrenheit = "&units=imperial&appid=";
-const connectorCelcius = "&units=metric&appid=";
-const key = "ab1be4aaa1e6745f9fcc41dbcf72d429";
-
-const WeatherChild = () => {
+const WeatherChild = (props) => {
   const [fahrenheit, setFahrenheit] = useState("");
-  const [celcius, setCelcius] = useState("");
-  const [toggle, setToggle] = useState(false);
+  const [uom , setUom] = useState('imperial');
 
-  const GetWeatherFahrenheit = () => {
-    const lat = "45.5808";
-    const lon = "-73.5825";
-    const fahrenheitURL = baseurl + lat + connector1 + lon + connectorFahrenheit + key;
-    console.log(fahrenheitURL);
+  useEffect(() => {
+      if (props.latitude != 0 && props.longitude != 0) {
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&units=${uom}&appid=ab1be4aaa1e6745f9fcc41dbcf72d429`
+        console.log(url);
+        fetch(url)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            setFahrenheit(data.main.temp);
+            console.log(fahrenheit);
+          })
+          .catch((err) => {
+            console.log("rejected:", err);
+          });
+      }
+  
+  }, [props.latitude, props.longitude, uom]); //[variable, variable, onChange]
 
-    fetch(fahrenheitURL)
-      .then((response) => {
-        console.log("resolved:", response);
-        return response.json();
-      })
-      .then((data) => {
-        setFahrenheit(data.main.temp);
-        console.log(fahrenheit);
-      })
-      .catch((err) => {
-        console.log("rejected:", err);
-      });
-
-    //   const GetWeatherCelcius = () => {
-    //     const lat = "45.5808";
-    //     const lon = "-73.5825";
-    //     const celciusURL = baseurl + lat + connector1 + lon + connectorCelcius + key;
-    //     console.log(celciusURL);
-    
-    //     fetch(celciusURL)
-    //       .then((response) => {
-    //         console.log("resolved:", response);
-    //         return response.json();
-    //       })
-    //       .then((data) => {
-    //         setCelcius(data.main.temp);
-    //         console.log(celcius);
-    //       })
-    //       .catch((err) => {
-    //         console.log("rejected:", err);
-    //       });
-  };
+function handleClick (){
+  uom === 'imperial' ? setUom ('metric') : setUom('imperial')
+}
 
   return (
     <Card>
-      <CardImg src={sunIcon} />
       <CardTitle>Weather</CardTitle>
-    <Button>{GetWeatherFahrenheit}</Button>
+      <CardText>{fahrenheit}</CardText>
+      <Button onClick={ handleClick } >Click for Celius/Fahrenheit</Button>
+
     </Card>
   );
 };
